868396548fcf1e841df892e04ab184c14e3b4ae9
SardineFish <sardine@live.cn>
2020-11-09 15:58:06 +0800

    Fix dependency issue.

--- packet/test-no-std/src/lib.rs
+++ packet/test-no-std/src/lib.rs
@@ -4,42 +4,74 @@
 #![feature(alloc_error_handler)]
 
 extern crate alloc;
+extern crate packet;
+extern crate rand;
 
 use core::alloc::GlobalAlloc;
 
-struct StupidHeep {
-}
+struct ExternAlloc {}
 
-impl StupidHeep {
-}
+impl ExternAlloc {}
 
-unsafe impl GlobalAlloc for StupidHeep {
+unsafe impl GlobalAlloc for ExternAlloc {
     unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
-        core::ptr::null_mut()
+        extern_alloc(layout.size())
     }
     unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        extern_dealloc(ptr)
     }
 }
 
-unsafe impl Sync for StupidHeep {}
+unsafe impl Sync for ExternAlloc {}
 
 #[global_allocator]
-static STUPID_HEAP: StupidHeep = StupidHeep {
-};
-
-fn main() {
-
-}
+static STUPID_HEAP: ExternAlloc = ExternAlloc {};
 
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
 
-#[panic_handler]
-fn my_panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+// #[panic_handler]
+// fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+//     loop {}
+// }
+
+// #[alloc_error_handler]
+// fn alloc_error(_layout: alloc::alloc::Layout) -> ! {
+//     loop {}
+// }
+
+extern "C" {
+    fn extern_alloc(size: usize) -> *mut u8;
+    fn extern_dealloc(ptr: *mut u8);
 }
 
-#[alloc_error_handler]
-    fn alloc_error(_layout: alloc::alloc::Layout) -> ! {
-        loop {}
+use packet::deserialize;
+use packet::packets::CapturedPacket;
+use packet::serialize;
+
+use rand::Rng;
+use rand::SeedableRng;
+
+#[no_mangle]
+extern "C" fn rust_main() {
+    let mut rng = rand::rngs::StdRng::seed_from_u64(12345);
+    let vec = alloc::vec::Vec::<i32>::with_capacity(100);
+    let mut payload = alloc::vec![0; 256];
+    for element in &mut payload {
+        *element = rng.gen();
     }
+    let packet = CapturedPacket {
+        source_ip: rng.gen(),
+        dest_ip: rng.gen(),
+        source_port: rng.gen(),
+        dest_port: rng.gen(),
+        protocol: rng.gen(),
+        payload: payload,
+    };
+
+    let mut buffer = [0; 8192];
+    serialize::serialize(&packet, &mut buffer);
+    let deserialized_packet = deserialize::deserialize::<CapturedPacket>(&buffer).unwrap();
+
+    assert_eq!(packet, deserialized_packet);
+}
--- packet/test-no-std/src/main.c
+++ packet/test-no-std/src/main.c
@@ -1,4 +1,23 @@
+#include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned char* extern_alloc(size_t size)
+{
+    printf("alloc for %ld bytes.\n", size);
+    return malloc(size);
+}
+
+unsigned char* extern_dealloc(unsigned char * ptr)
+{
+    printf("dealloc memory at 0x%llx.\n", ptr);
+    free(ptr);
+}
+
+extern void rust_main();
+
 int main()
 {
+    rust_main();
     return 0;
 }
